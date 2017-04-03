6b29b327dc3f1ebb113756cebe3c6d7bfcd199bb
Nvertigo <nvertigo67@gmail.com>
2017-04-03 09:56:01 +0200

    remove repopicking remove_call_barring: merged upstream.

--- my_pick.sh
+++ my_pick.sh
@@ -1,14 +1,11 @@
 #!/bin/bash
 
-. build/envsetup.sh
+#. build/envsetup.sh
 
 #temp fix for ogg/vorbis
 pushd hardware/qcom/audio-caf/msm8996
 git fetch https://review.lineageos.org/LineageOS/android_hardware_qcom_audio refs/changes/64/163564/3 && git cherry-pick FETCH_HEAD
 popd
-
-#stop sim settings from crashing
-repopick -t remove_call_barring
 
 #add Magisk
 pushd build
