b63bc3cad3d5d30adc7d144ee5cc705b7c22f800
Nvertigo <nvertigo67@gmail.com>
2017-04-09 11:32:09 +0200

    Revert "revert April fool jokes."
    This reverts commit 9900cbbccefa97269ad1b0f755642b6c8a9e04f6.

--- my_pick.sh
+++ my_pick.sh
@@ -7,12 +7,6 @@ pushd hardware/qcom/audio-caf/msm8996
 git fetch https://review.lineageos.org/LineageOS/android_hardware_qcom_audio refs/changes/64/163564/3 && git cherry-pick FETCH_HEAD
 popd
 
-#revert April fool jokes
-pushd vendor/cm
-git fetch https://review.lineageos.org/LineageOS/android_vendor_cm refs/changes/78/167678/1 && git cherry-pick FETCH_HEAD
-git fetch https://review.lineageos.org/LineageOS/android_vendor_cm refs/changes/79/167679/1 && git cherry-pick FETCH_HEAD
-popd
-
 #add Magisk
 pushd build
 wget https://github.com/nvertigo/android_build/commit/4bd296c062c8d17ec6e443c5fed46dc3c1813654.patch -O - | git am
