67ef35415d182a7d7d25da663faed2170ceb3f37
Nvertigo <nvertigo67@gmail.com>
2017-04-17 19:42:19 +0200

    Revert "add: base: services: add config_fingerprintRemoveClientOnCancel."
    This reverts commit f74b927f165d669184ad6ba538a7170f9d9a0c6d.

--- my_pick.sh
+++ my_pick.sh
@@ -16,6 +16,4 @@ popd
 pushd frameworks/base
 wget https://github.com/nvertigo/android_frameworks_base/commit/9289822544678bb9f11d24da413ec4a9b9636694.patch -O - | git am
 wget https://github.com/nvertigo/android_frameworks_base/commit/24f384ff3e16742a722d101981f84422ba452a99.patch -O - | git am
-#base: services: add config_fingerprintRemoveClientOnCancel
-wget https://github.com/nvertigo/android_frameworks_base/commit/eef4a2268684699d23f82e1bb9c0707151839ff1.patch -O - | git am
 popd
