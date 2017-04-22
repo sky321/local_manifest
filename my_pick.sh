182594560641e449c0fa04ecfd18e87656fd68c5
Nvertigo <nvertigo67@gmail.com>
2017-04-22 10:54:09 +0200

    my-pick.sh: add comments to magisk commits.

--- my_pick.sh
+++ my_pick.sh
@@ -9,7 +9,9 @@ popd
 
 #add Magisk
 pushd build
+#Build: Switch to Magisk as the built in root
 wget https://github.com/nvertigo/android_build/commit/63a305e3312595f73d4e037e66bdfed87ecc1d20.patch -O - | git am
+#magisk: delete magisk.zip from system after flashing
 wget https://github.com/nvertigo/android_build/commit/ab9e918cfabb27f9895d1738af2cf226708361a5.patch -O - | git am
 popd
 
