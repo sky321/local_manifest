0e088ebb806e303a6a23c791b10a76312d6a9adf
Nvertigo <nvertigo67@gmail.com>
2017-04-27 15:57:06 +0200

    my_pick.sh: add "Press ENTER..."; update URLs after rebase.
    This gets complecated: I added breaks after each patching
    block, to make it possible to watch for conflicts and resolve
    them before the script continues.

--- my_pick.sh
+++ my_pick.sh
@@ -20,6 +20,8 @@ popd
 
 . build/envsetup.sh
 
+read -p "Press ENTER..."
+
 #add Magisk
 pushd build
 #Build: Switch to Magisk as the built in root
@@ -28,18 +30,30 @@ wget https://github.com/nvertigo/android_build/commit/63a305e3312595f73d4e037e66
 wget https://github.com/nvertigo/android_build/commit/5bd6eb23466b8c0b13142b20360fc6e4670e3ba5.patch -O - | git am
 popd
 
+read -p "Press ENTER..."
+
 pushd frameworks/base
 #microG
 #Add signature Spoofing permission
-wget https://github.com/nvertigo/android_frameworks_base/commit/07315b1fc6cf0ff013a88c53608817ea02109189.patch -O - | git am
+wget https://github.com/nvertigo/android_frameworks_base/commit/029c7d7f1111d6f1cc9d90f6626b49e372afe9d2.patch -O - | git am
 #Allow location providers also outside of /system
-wget https://github.com/nvertigo/android_frameworks_base/commit/14f700ad41c6b3ad2d21d2311d66e74b20990b99.patch -O - | git am
+wget https://github.com/nvertigo/android_frameworks_base/commit/a58a27b7c24f95308f64618ff751e3179b9ca436.patch -O - | git am
+popd
+
+read -p "Press ENTER..."
+
+repopick -t network-traffic
+
+read -p "Press ENTER..."
+
+pushd frameworks/base
 #DASH
-#indicate this
+#BatteryService: add dash charging support
 wget https://github.com/nvertigo/android_frameworks_base/commit/afce974d9a456fd1e00a469b5ce9cddb811ccb93.patch -O - | git am
 popd
 
-repopick -t network-traffic
+
+read -p "Press ENTER..."
 
 # get the LineageOMS commits:
 ${LOMSDIR}/lineage_oms_merge.sh ${LAOSROOT}
