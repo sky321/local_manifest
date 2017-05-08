d0b7876163af3d9e1d469eb020093e5e1f3a15e3
Nvertigo <nvertigo67@gmail.com>
2017-05-08 12:08:41 +0200

    my_pick.sh: don't patch Magisk in build/: moved to device tree.

--- my_pick.sh
+++ my_pick.sh
@@ -22,16 +22,6 @@ popd
 
 read -p "Press ENTER..."
 
-#add Magisk
-pushd build
-#Build: Switch to Magisk as the built in root
-wget https://github.com/nvertigo/android_build/commit/63a305e3312595f73d4e037e66bdfed87ecc1d20.patch -O - | git am
-#magisk: delete magisk.zip from system after flashing
-wget https://github.com/nvertigo/android_build/commit/5bd6eb23466b8c0b13142b20360fc6e4670e3ba5.patch -O - | git am
-popd
-
-read -p "Press ENTER..."
-
 pushd frameworks/base
 #microG
 #Add signature Spoofing permission
