effbe183ac8f99cd04eef1a2baa8dc31312e7ce8
Nvertigo <nvertigo67@gmail.com>
2018-10-31 13:09:04 +0100

    NLOS: SDCLANG-6: add patches and patching script for display-caf and media-caf.

---
+++ sdclang-patch.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+
+BUILD_ROOT=/usr/local/src/los15
+LOCAL_MANIFEST=my_tmp/local_manifest
+
+cd ${BUILD_ROOT}
+
+patch -p1 ${LOCAL_MANIFEST}/display-caf-8996.diff
+patch -p1 ${LOCAL_MANIFEST}/media-caf-8996.diff
