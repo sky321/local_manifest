ecb8f478f212d1be774ba6e0ca65c54c20dc319e
Nvertigo <nvertigo67@gmail.com>
2018-10-31 13:21:01 +0100

    NLOS: sdclang-patch.sh: fix derp.

--- sdclang-patch.sh
+++ sdclang-patch.sh
@@ -5,5 +5,5 @@ LOCAL_MANIFEST=my_tmp/local_manifest
 
 cd ${BUILD_ROOT}
 
-patch -p1 ${LOCAL_MANIFEST}/display-caf-8996.diff
-patch -p1 ${LOCAL_MANIFEST}/media-caf-8996.diff
+patch -p1 <${LOCAL_MANIFEST}/display-caf-8996.diff
+patch -p1 <${LOCAL_MANIFEST}/media-caf-8996.diff
