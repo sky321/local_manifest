d4ce68ee786d536bd6b2148baf3f32a4ca531b81
Nvertigo <nvertigo67@gmail.com>
2018-10-31 11:52:28 +0100

    NLOS: add update_repos.sh for rebasing our own repos upon lineage.

---
+++ update_repos.sh
@@ -0,0 +1,42 @@
+#!/bin/bash
+
+GITUSER="nvertigo"
+
+REPOS="
+build/make
+device/qcom/sepolicy
+frameworks/av
+frameworks/base
+frameworks/native
+packages/apps/Gallery2
+packages/apps/LockClock
+packages/apps/Settings
+packages/apps/WallpaperPicker
+system/sepolicy
+vendor/lineage"
+
+for i in $REPOS; do
+  pushd $i
+
+  git fetch los lineage-15.1
+  if [ "$?" -gt "0" ] ; then
+    read -p "$i: fetch los lineage-15.1 failed. STRG-C to abort. ENTER to continue"
+  fi
+
+  git checkout nlos-15.1
+  if [ "$?" -gt "0" ] ; then
+    read -p "$i: checkout nlos-15.1 failed. STRG-C to abort. ENTER to continue"
+  fi
+
+  git rebase los/lineage-15.1
+  if [ "$?" -gt "0" ] ; then
+    read -p "$i: rebase on top of los/lineage-15.1 failed. STRG-C to abort. ENTER to continue"
+  fi
+
+  git push -f 
+  if [ "$?" -gt "0" ] ; then
+    read -p "$i: push -f to $GITUSER failed. STRG-C to abort. ENTER to continue"
+  fi
+
+  popd
+done
