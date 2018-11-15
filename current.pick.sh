707352d5f1a159731535bdba047a2af952ee51a5
Nvertigo <nvertigo67@gmail.com>
2018-11-15 21:12:09 +0100

    NLOS: current.pick.sh: remove "libdisplayconfig: Depend on vendor.display.config@1.0_vendor"; merged upstream.

--- current.pick.sh
+++ current.pick.sh
@@ -19,11 +19,6 @@ repopick https://review.lineageos.org/#/c/LineageOS/android_packages_apps_Lineag
 repopick https://review.lineageos.org/#/c/LineageOS/android_frameworks_base/+/221716/18 -P frameworks/base/
 repopick 219299 -P packages/apps/Settings/
 
-#libdisplayconfig: Depend on vendor.display.config@1.0_vendor
-repopick 232229 -P hardware/qcom/display-caf/msm8996/
-repopick 232228 -P hardware/qcom/display-caf/sdm845/
-repopick 232227 -P hardware/qcom/display-caf/msm8998/
-
 #Merge tag 'LA.UM.6.6.r1-09900-89xx.0' 
 repopick 232913 -P hardware/qcom/audio-caf/msm8996/
 repopick 232914 -P hardware/qcom/display-caf/msm8996/
