a4a27a5c19423f4823accd85f552ef4c5a0846d4
Nvertigo <nvertigo67@gmail.com>
2019-01-09 10:30:09 +0100

    Revert "current.pick.sh: "op3: Update FOSS props for Pie"."
      Merged upstream.
    
    This reverts commit ae8551f79e4ff5844c547415883bc5d05b46ad8e.

--- current.pick.sh
+++ current.pick.sh
@@ -56,9 +56,6 @@ repopick 237209 -P vendor/lineage/
 # config: Add Deskclock to power whitelist
 repopick 238057 -P vendor/lineage/
 
-# op3: Update FOSS props for Pie
-repopick 238432 -P device/oneplus/oneplus3/
-
 # softap: sdk: Add 'vendor.' prefix to wlan.driver.config
 repopick 237859
 
