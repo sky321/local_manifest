ceb595477bd1c9f3bd71db2c18b694c792b89c48
Nvertigo <nvertigo67@gmail.com>
2019-02-06 20:27:24 +0100

    Revert "current.pick.sh: Revert "AOSP/Messaging: bump targetSDK to 28"."
    This reverts commit 4b33485ad7e43554232f7259564d416cc1e90ce1.

--- current.pick.sh
+++ current.pick.sh
@@ -55,9 +55,6 @@ safer-repopick 233633 -P frameworks/base/
 safer-repopick 233634 -P packages/apps/Settings/
 safer-repopick 237209 -P vendor/lineage/
 
-# Revert "AOSP/Messaging: bump targetSDK to 28"
-safer-repopick 238551
-
 # libril: Fix manual network selection with old modem & op3: Enable workaround for old MCC/MNC format
 safer-repopick 230737
 safer-repopick 240026 -P device/oneplus/oneplus3/
