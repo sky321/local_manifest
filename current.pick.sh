da2ace81081761144fff241a96623851b6138d15
Nvertigo <nvertigo67@gmail.com>
2019-02-08 19:57:54 +0100

    current.pick.sh: "op3: Migrate to livedisplay 2.0".

--- current.pick.sh
+++ current.pick.sh
@@ -73,3 +73,6 @@ safer-repopick 241212 -P device/oneplus/oneplus3/
 
 # op3: Convert KeyDisabler to touch HAL
 safer-repopick 241213 -P device/oneplus/oneplus3/
+
+# op3: Migrate to livedisplay 2.0
+safer-repopick 240885 -P device/oneplus/oneplus3/
