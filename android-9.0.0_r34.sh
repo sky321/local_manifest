419371314d243d1175f60e5408e33269ba3ec495
Nvertigo <nvertigo67@gmail.com>
2019-03-08 13:55:34 +0100

    Revert "Patch up to android-9.0.0_r34."
      Merged upstream.
    
    This reverts commit ff7ffdc0464a4b6b25d1818efbfa7fba450f01cd.

--- android-9.0.0_r34.sh
+++
@@ -1,5 +0,0 @@
-repopick -t android-9.0.0_r34 -e 243426,243427,243428,243431
-repopick 243426 -P build/make/
-repopick 243427 -P frameworks/av/
-repopick 243428 -P frameworks/base/
-repopick 243431 -P packages/apps/PackageInstaller/
