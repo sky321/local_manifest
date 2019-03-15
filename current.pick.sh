299722823f202198d35eea2eaf5912be6181d7e5
Nvertigo <nvertigo67@gmail.com>
2019-03-15 10:00:47 +0100

    Revert "current.pick.sh: p_webview_72.0.3626.121."
      Merged upstream.

--- current.pick.sh
+++ current.pick.sh
@@ -77,9 +77,6 @@ safer-repopick -t fix-aapt-warnings -e 242678,242682
 safer-repopick 242678 -P packages/apps/Messaging/
 safer-repopick 242682 -P packages/apps/PackageInstaller/
 
-# p_webview_72.0.3626.121 for x86 - arm merged
-safer-repopick 243685
-
 # unclipped_layers
 safer-repopick -t unclipped_layers -e 243693
 safer-repopick 243693 -P frameworks/base/
