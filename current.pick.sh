4724761c269da1c67071140bc77a58b8ae35eb16
Nvertigo <nvertigo67@gmail.com>
2019-03-23 14:18:15 +0100

    Revert "current.pick.sh: lineagehw-hidl."
      Merged upstream.
    
    This reverts commit e20d455b03cd705bd64364d4802ef0c563098e6a.

--- current.pick.sh
+++ current.pick.sh
@@ -51,11 +51,6 @@ safer-repopick 233633 -P frameworks/base/
 safer-repopick 233634 -P packages/apps/Settings/
 safer-repopick 237209 -P vendor/lineage/
 
-# lineagehw-hidl
-safer-repopick 241116
-safer-repopick -t lineagehw-hidl -e 241116,241004
-safer-repopick 241004 -P lineage-sdk/
-
 # lineage-sepolicy-fix & op3: Rewrite livedisplay HAL sepolicy
 safer-repopick -t lineage-sepolicy-fix
 #safer-repopick 241763 -P device/oneplus/oneplus3/
